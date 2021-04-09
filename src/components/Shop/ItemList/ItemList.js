afa676067b4af1795de8173b604eb97bb58d1057
Bogomil Anchev <bogomilanchev@gmail.com>
2021-04-09 12:38:44 +0300

    item list search implemented

--- src/components/Shop/ItemList/ItemList.js
+++ src/components/Shop/ItemList/ItemList.js
@@ -1,7 +1,7 @@
 import "./ItemList.scss";
 import firebase from "../../../services/firebase";
 import { useState, useEffect } from "react";
-import Item from '../Item/Item'
+import Item from "../Item/Item";
 
 function ItemList(params) {
    const [products, setProducts] = useState([]);
@@ -14,21 +14,46 @@ function ItemList(params) {
          .then((data) => {
             let arr = [];
             data.forEach((doc) => {
-                let info = doc.data();               
+               let info = doc.data();
                arr.push({
-                   id: doc.id,
-                   info: info
+                  id: doc.id,
+                  info: info,
                });
             });
             setProducts(arr);
          });
    }, []);
 
+   const search = (e) => {
+      let searchValue = e.target.value;
+      firebase
+         .firestore()
+         .collection("products")
+         .get()
+         .then((data) => {
+            let arr = [];
+            data.forEach((doc) => {
+               let info = doc.data();
+               arr.push({
+                  id: doc.id,
+                  info: info,
+               });
+            });
+            let filteredProducts = arr.filter((x) => {
+               return x.info.title.toLowerCase().includes(searchValue.toLowerCase());
+            });
+            setProducts(filteredProducts);
+         });
+   };
+
    return (
-      <section className="shop-list">
-         {products.map(product => {
-             return <Item key={product.id} id={product.id} item={product.info} />
-         })}
+      <section className="shop-section">
+         <input onKeyUp={search} type="text" placeholder="Search"></input>
+         <article className="shop-section-list">
+            {products.map((product) => {
+               return <Item key={product.id} id={product.id} item={product.info} />;
+            })}
+         </article>
       </section>
    );
 }
