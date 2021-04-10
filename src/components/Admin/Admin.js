5cf9c538de9f290045ff446be7ddc09671b96168
Bogomil Anchev <bogomilanchev@gmail.com>
2021-04-10 16:49:40 +0300

    admin route guard

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,15 +1,24 @@
 import "./Admin.scss";
+import UserContext from "../../context/UserContext";
+import { useContext } from "react";
+import { Redirect } from "react-router-dom";
 import CreateEditProduct from "./CreateEditProduct";
-import ItemList from "../Shop/ItemList/ItemList"
+import ItemList from "../Shop/ItemList/ItemList";
 
 function Admin({ history }) {
-   return (
-      <section className='admin'>
-         <CreateEditProduct history={history} doEdit="false" />
-         <h2>Edit and Delete products</h2>
-         <ItemList isAdmin='true' />
-      </section>
-   );
+   const user = useContext(UserContext);
+
+   if (!user) {
+      return <Redirect to="/softuni-react-exam/login" />;
+   } else {
+      return (
+         <section className="admin">
+            <CreateEditProduct history={history} doEdit="false" />
+            <h2>Edit and Delete products</h2>
+            <ItemList isAdmin="true" />
+         </section>
+      );
+   }
 }
 
 export default Admin;
